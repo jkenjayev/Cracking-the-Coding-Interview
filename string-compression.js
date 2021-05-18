cbfb6e2ec263d5894cca3d25d6de6b9feeced1ce
jkenjayev <javohirwebdev@gmail.comm>
2021-05-18 13:56:36 +0500

    compressed is not appearned but code is correnct

---
+++ string-compression.js
@@ -0,0 +1,19 @@
+function compress(rawString) {
+    let count = 0;
+    let compressedString = "";
+    for(let i = 0; i < rawString.length; i++) {
+        count++;
+        const currentLetter = rawString[i];
+        if (currentLetter !== rawString[i+1]) {
+            compressedString += currentLetter + count;
+            count = 0;
+        }
+
+        return compressedString.length < rawString.length ? compressedString : rawString;
+    }
+}
+
+let rawString = "aaabbcccd";
+console.log(`Raw: ${rawString}`);
+const compressed = compress(rawString);
+console.log(`Compressed: ${compressed}`);
